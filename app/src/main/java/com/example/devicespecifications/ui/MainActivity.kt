a929a7b51b6b35e7edcfc89849e29275c7a48b2a
andronicus_kim <androkim.kim7@gmail.com>
2022-05-31 15:23:57 +0300

    implements scanning device, showing components and adds some icons

--- app/src/main/java/com/example/devicespecifications/ui/MainActivity.kt
+++ app/src/main/java/com/example/devicespecifications/ui/MainActivity.kt
@@ -2,14 +2,22 @@ package com.example.devicespecifications.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.devicespecifications.R
+import com.example.devicespecifications.data.Component
 import com.example.devicespecifications.data.ComponentDetail
+import com.example.devicespecifications.data.Components
 import com.example.devicespecifications.databinding.ActivityMainBinding
 import com.example.devicespecifications.utils.Constants
+import com.example.devicespecifications.utils.DeviceInfo
+import com.example.devicespecifications.utils.HelperFunctions
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
 import kotlin.coroutines.CoroutineContext
@@ -28,9 +36,48 @@ class MainActivity : AppCompatActivity(), CoroutineScope, ComponentAdapter.OnCom
         job = Job() // create the Job
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // handle scan button click
+        binding.btnScan.setOnClickListener {
+            binding.pbLoadingIndicator.visibility = View.VISIBLE
+            scanDeviceShowAndSaveDetails()
+        }
+    }
+    private fun setupAdapter(){
+        // add list of components/hardware to adapter
+        val adapter = ComponentAdapter(
+            listOf(
+                Component(R.drawable.ic_system, Components.SYSTEM.name),
+                Component(R.drawable.ic_device,Components.DEVICE.name),
+                Component(R.drawable.ic_processor,Components.PROCESSOR.name),
+                Component(R.drawable.ic_memory,Components.STORAGE.name),
+                Component(R.drawable.ic_battery,Components.BATTERY.name),
+                Component(R.drawable.ic_network,Components.NETWORK.name),
+                Component(R.drawable.ic_camera,Components.CAMERA.name),
+            ),this)
+        binding.rvComponents.adapter = adapter
+    }
+
+    private fun scanDeviceShowAndSaveDetails(){
+        launch {
+            // Scan device details in a background thread to prevent freezing the UI
+            componentsDetails = withContext(Dispatchers.IO) {
+                HelperFunctions.scanDevice(DeviceInfo(this@MainActivity))
+            }
+            // set up adapter to render components
+            setupAdapter()
+            writeToFile()
+            binding.tvEmptyList.visibility = View.GONE
+            binding.pbLoadingIndicator.visibility = View.GONE
+            binding.tvComponentsHeader.visibility = View.VISIBLE
+            binding.rvComponents.visibility = View.VISIBLE
+        }
     }
     private fun writeToFile(){
+        /*
+            * Create and Write device specifications into a file(device_specs.txt)
+            * */
         val data = StringBuilder()
+        // Build a string of all component details
         componentsDetails.forEach {
             data.append(it.title)
             data.append("\n")
