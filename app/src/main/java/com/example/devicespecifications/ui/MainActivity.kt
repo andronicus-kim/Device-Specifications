18a8ec8e8c91018cbb969397e9dc6d33c5261f78
andronicus_kim <androkim.kim7@gmail.com>
2022-05-31 15:11:50 +0300

    coroutinescope and viewbinding implementation

--- app/src/main/java/com/example/devicespecifications/ui/MainActivity.kt
+++ app/src/main/java/com/example/devicespecifications/ui/MainActivity.kt
@@ -3,10 +3,28 @@ package com.example.devicespecifications.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.devicespecifications.R
+import com.example.devicespecifications.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    private lateinit var job: Job
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        job = Job() // create the Job
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    override fun onDestroy() {
+        job.cancel() // cancel the Job
+        super.onDestroy()
     }
 }
