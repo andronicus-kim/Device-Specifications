40154963c3c124e996999a3eef838417cbf6a9c8
andronicus_kim <androkim.kim7@gmail.com>
2022-05-31 15:05:49 +0300

    clean up

--- app/src/main/java/com/example/devicespecifications/ui/MainActivity.kt
+++ app/src/main/java/com/example/devicespecifications/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.devicespecifications
+package com.example.devicespecifications.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.devicespecifications.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
